368f12b63f54103abd8265157bef6de70f387363
MoisesGodoy17 <moises.godoy@usach.cl>
2022-09-24 19:08:36 -0300

    add depthLayer + compress?

--- imagen-constructor.rkt
+++ imagen-constructor.rkt
@@ -73,9 +73,9 @@
     (es-bitmap? (descontructor imagen)))); cambia el formato de la imagen, dejando todos los pixeles en una lista -> '((0 10)(1 20)(1 40)(1 8))
 
 ;Descripcion: Funcion que determina si es un bitmap o no. esta recorre toda la imgen y determina si los pixeles
-;corresponden al un pixbit, si llega al final de la imagen, significa que la imagen es un bitmap
+;corresponden a un pixbit, si llega al final de la imagen, significa que la imagen es un bitmap
 ;y retorna #t de no ser asi retorna #f.
-;Dominio: Imagen
+;Dominio: List
 ;Recorrido: Boolean
 ;Tipo de recursion: Recursion de cola
 
@@ -94,10 +94,10 @@
   (lambda (imagen)
     (es-pixmap? (descontructor imagen))))
 
-;Descripcion: Funcion que determina si es un pixrgb o no. esta recorre toda la imgen y determina si los pixeles
-;corresponden al un pixrgb, si llega al final de la imagen, significa que la imagen es un pixmap
+;Descripcion: Funcion que determina si es un pixmap o no. esta recorre toda la imgen y determina si los pixeles
+;corresponden a un pixrgb, si llega al final de la imagen, significa que la imagen es un pixmap
 ;y retorna #t de no ser asi retorna #f.
-;Dominio: Imagen
+;Dominio: List
 ;Recorrido: Boolean
 ;Tipo de recursion: Recursion de cola
 
@@ -108,10 +108,20 @@
       [(eq? #f (es-pixrgb? (car imagen-descontruida) 0 (cant-elementos-pixrgb (car imagen-descontruida)))) #f]
       (else (es-pixmap? (cdr imagen-descontruida))))))
 
+;Descripcion: funcion envoltorio de es-maphex?
+;Recorrido: Imagen
+;Dominio: Boolean
 
 (define maphex?
   (lambda (imagen)
     (es-maphex? (descontructor imagen))))
+
+;Descripcion: Funcion que determina si es un pixrgb o no. esta recorre toda la imgen y determina si los pixeles
+;corresponden a un pixmap, si llega al final de la imagen, significa que la imagen es un pixmap
+;y retorna #t de no ser asi retorna #f.
+;Dominio: List
+;Recorrido: Boolean
+;Tipo de recursion: Recursion de cola
 
 (define es-maphex?
   (lambda (imagen-descontruida)
@@ -123,58 +133,107 @@
          (else #f))]
       (else #f))))
 
-
 ;(imagen 2 2 (pixhex-d  0 0 "#AF00AA" 10) (pixhex-d  0 1 "#FF00FF" 20) (pixhex-d 1 0 "#00AAEE" 30) (pixhex-d 1 1 "#DBFF00" 4))
 ;(maphex? (imagen 2 2 (pixhex-d  0 0 "#AF00AA" 10) (pixhex-d  0 1 "#FF00FF" 20) (pixhex-d 1 0 "#00AAEE" 30) (pixhex-d 1 1 "#DBFF00")))
 
-;Descripcion: funcion envoltorio de flipV-interna
-;Dominio: lista
-;Recorrido: imagen
+;Descripcion: funcion envoltorio de compressed-envo
+;Dominio: Imagen
+;Recorrido: Boolean
+
+(define compressed?
+  (lambda (image)
+    compressed-envo (descontructor image) (get-cant-col imagen) (get-cant-fil imagen) 0))
+
+;Descripcion: funcion que determina si una imagen esta comprimida o no. Para hacerlo cuenta los elementos que hay
+;en la imagen, si estos son iguales a producto de "* fil col" ebtoces no esta comprimida y retorna #f, en caso contrario
+;retornara un #f
+;Dominio: Imagen X Num X Num X Num
+;Recorrido: Boolean
+;Tipo de recursion: Recursion de cola
+
+(define compressed-envo
+  (lambda (imagen fil col contador)
+    (cond
+      [(null? imagen)
+       (cond
+         [((< contador (* fil col)) #t)];si la cantidad de elementos que hay en la imagen es menor al las que forman * fil col, entoces esta comprimida (#t)
+         (else #f))] ;si la cantidad es igual, entonces es una imagen sin modificar
+      (else (compressed-envo (cdr imagen) (+ 1 contador)))))) ;suma los elementos de la lista de pixeles
+  
+;Descripcion: funcion envoltorio de flipV-envo
+;Dominio: List
+;Recorrido: Imagen
 
 (define flipV ;funcion flipV-> recive la imagen e invierte las filas
   (lambda (imagen)
-    (flipV-interna imagen '())))
+    (flipV-envo imagen '())))
 
 ;Descripcion: funcion que invierte el orden de las filas haciendo uso de la recursion de cola. Se pasan las filas de
 ;la imagen original a una imagen aux, al hacerlo con recursion de cola lo que antes era la primera fila en la nueva
 ;imagen sera la ultima y asi sucesivamente, hasta retornar una image invertida de forma no declarativa
-;Dominio: imagen
-;Recorrido: imagen
+;Dominio: Imagen X List
+;Recorrido: Imagen
 ;Tipo de recursion: Recursion de cola
 
-(define flipV-interna
+(define flipV-envo
   (lambda (imagen flip-list)
     (cond
       [(null? imagen) flip-list]; retorno la nueva imagen invertida
-      (else (flipV-interna (cdr imagen) (cons (car imagen) flip-list)))))); paso lo elementos a una nueva imagen
+      (else (flipV-envo (cdr imagen) (cons (car imagen) flip-list)))))); paso lo elementos a una nueva imagen
+
+;Descripcion: funcion envoltorio de get-filas
+;Dominio: Imagen
+;Recorrido: Imagen
 
 (define flipH
   (lambda (imagen)
     (get-filas imagen '())))
 
+;Descripcion: funcion que invierte las filas de una imagen, al hacerlo llamara a la funcion flipV-envo que hara el trabajo de
+;invertir la imagen final.
+;Dominio: Imagen X List
+;Recorrido: Imagen
+;Tipo de recursion: de cola
+
 (define get-filas;;get
   (lambda (imagen flip-list)
           (cond
-            [(null? imagen) (flipV-interna flip-list '())];;llamo a la funcion flipV, esta me invierte la lista
+            [(null? imagen) (flipV-envo flip-list '())];;llamo a la funcion flipV, esta me invierte la lista
             (else (get-filas (cdr imagen) (cons (flipH-invierte-filas (car imagen) '()) flip-list))))))
+
+;Descripcion: funcion que invierte los elementos de las filas, uno a uno, haciendo uso de la recursion de cola.
+;Dominio: Lista X List
+;Recorrido: Lista
+;Tipo de recursion: de cola
 
 (define flipH-invierte-filas
   (lambda (filas list-aux)
     (cond
-      [(null? filas) list-aux]
-      (else (flipH-invierte-filas (cdr filas) (cons (car filas) list-aux))))))
+      [(null? filas) list-aux];retorna mi lista con los elementos invertidos
+      (else (flipH-invierte-filas (cdr filas) (cons (car filas) list-aux)))))); copia los elementos de la lista de forma invertida
 
 ;---getters---;
+
+;Descripcion: obtiene la cantidad de columnas que hay
+;Dominio: Imagen
+;Recorrido: Num
+;Tipo de recursion: Natural
 
 (define get-cant-col;;get cololumnas ***
   (lambda (imagen)
     (cond
       [(null? imagen) 0]
-      (else (+ 1 (get-cant-col (cdr imagen)))))))
+      (else (+ 1 (get-cant-col (cdr imagen))))))); suma 1 mientras no sea NULL
+;recorre la imagen fila a fila, de esa forma retornara la cantidad de columnas que hay.
+
+;Descripcion: obtiene la cantidad de filas que hay, haciendo uso de la funcion "get-cant-col"
+;Dominio: Imagen
+;Recorrido: Num
 
 (define get-cant-fil;;get filas ***
   (lambda (imagen)
-    (get-cant-col (car imagen))))
+    (get-cant-col (car imagen)))) ; le doy la fila entera de imagen ej: ((1 1)(1 0)(0 0))-> de esta froma recorre es lista
+;y retorna la cantidad de elementos que hay en una fila.
 
     
 ;;(flipV (constructor-imagen 2 2 (pixbit-d  0 0 1 10) (pixbit-d  0 1 0 20) (pixbit-d 1 0 0 30) (pixbit-d 1 1 1 4)))
@@ -183,10 +242,17 @@
 
 
 ;;funcion que recorta el cuadrante de una imagen y retorna una imagen recortada
-;;x es y e y es x, esta al revez
+
+;Descripcion: funcion que selecciona una cuadrante y retorna los pixeles que hay en dicho cuadrante
+;Dominio: Imagen
+;Recorrido: Imagen
+;Tipo de recursion: envoltorio de la funcion toma-pixeles
+
 (define crop
-  (lambda (imagen x1 y1 x2 y2)
+  (lambda (imagen x1 y1 x2 y2);X es Y e Y es X, esta al revez
     (toma-pixeles imagen x1 y1 x2 y2 '() (get-cant-col imagen) (get-cant-fil imagen) 0 0)))
+
+
 
 (define toma-pixeles
   (lambda (imagen x1 y1 x2 y2 recorte fil col cant-col cant-fil)
@@ -215,22 +281,41 @@
 ;(provide get-G)
 ;(provide get-B)
 
+;Descripcion: funcion que tranforma una imagen con representacion de pixeles pixrgb a una imagen
+;representada por hexpix, haciendo uso de funciones del "TDA-pixrgb-d"
+;Dominio: Imagen
+;Recorrido: Imagen
+;Tipo de recursion: envoltorio de la funcion "get-filas-rgb"
+
 (define imgRGB->imgHex
   (lambda (imagen)
-    (get-columnas imagen '())))
+    (get-filas-rgb imagen '())));;funion que obtiene las filas de una imagen
 
-(define get-columnas
+;Descripcion: funcion que extare las filas de una imagen pixmap y crea una nueva imagen con una representacion de hexmap
+;donde los pixeles de la imagen son son pixhex.
+;Dominio: Imagen X List
+;Recorrido: Imagen
+;Tipo de recursion: de cola
+
+(define get-filas-rgb
   (lambda (imagen lista-temp)
     (cond
-      [(null? imagen) (reverse lista-temp)]
-      (else (get-columnas (cdr imagen) (cons (recorre-pixeles (car imagen) '()) lista-temp))))))
+      [(null? imagen) (reverse lista-temp)]; la invierte producto de la recursion de cola
+      (else (get-filas-rgb (cdr imagen) (cons (recorre-pixeles (car imagen) '()) lista-temp)))))); toma la fila de una imagen
+
+;Descripcion: funcion que pasa tranforma un pixel rgbpix a un pixel pixhex, tranformando las componentes
+;de RGB a hex, haciendo uno de las funciones alojadas en "TDA-Pixrgb-d".
+;donde los pixeles de la imagen son son pixhex.
+;Dominio: List X List
+;Recorrido: List
+;Tipo de recursion: de cola
 
 (define recorre-pixeles
-  (lambda (columna lista)
+  (lambda (fila lista)
     (cond
-      [(null? columna) (reverse lista)]
-      (else (recorre-pixeles (cdr columna) (cons (cons (string-append "#"
-      (string-append (car (resto-num (get-R (car columna)) '())) (string-append (car (resto-num (get-G (car columna)) '())) (car (resto-num (get-B (car columna)) '()))))) (list (get-D (car columna)))) lista))))))
+      [(null? fila) (reverse lista)] ;retorma una lista o fila con pixhex
+      (else (recorre-pixeles (cdr fila) (cons (cons (string-append "#" ;extrae las componentes de la imagen y la tranforma a numeros hex, luego une las componenetes formando un pixhex
+      (string-append (car (resto-num (get-R (car fila)) '())) (string-append (car (resto-num (get-G (car fila)) '())) (car (resto-num (get-B (car fila)) '()))))) (list (get-D (car fila)))) lista))))))
 
 
 ;;(imgRGB->imgHex (constructor-imagen 2 2 (pixrgb-d  0 0 10 10 10 10) (pixrgb-d  0 1 20 20 20 20) (pixrgb-d 1 0 30 30 30 30) (pixrgb-d 1 1 40 40 40 40)))
@@ -238,28 +323,51 @@
 ;;funcion que unifica todas las columnas, dejando una imagen de una columna y una fila con conpuesta por todas las columnas
 ;;recursion natural
 
+;Descripcion: funcion que deconstruye una imagen a una lista de pixeles, pasa lso pixeles de la imagen a una lista,
+;perdiendo el formato dado por la funcion "image"
+;donde los pixeles de la imagen son son pixhex.
+;Dominio: Image
+;Recorrido: List
+;Tipo de recursion: Natural
+
 (define descontructor
   (lambda (imagen)
     (cond
-     [(null? imagen) '()]
-     (else (append (get-filas-def (car imagen)) (descontructor (cdr imagen)))))))
+     [(null? imagen) '()]; caso base
+     (else (append (get-filas-def (car imagen)) (descontructor (cdr imagen))))))); llama a la funcion "gets-filas-def" y une el contenido a una nueva lista
+
+;Descripcion: funcion que recive una fila y retorna una lista con los pixeles
+;Dominio: List
+;Recorrido: List
+;Tipo de recursion: Natural
 
 (define get-filas-def
   (lambda (fila-pixeles)
     (cond
-      [(null? fila-pixeles) '()]
-      (else (cons (car fila-pixeles) (get-filas-def (cdr fila-pixeles)))))))
+      [(null? fila-pixeles) '()] ;caso base
+      (else (cons (car fila-pixeles) (get-filas-def (cdr fila-pixeles))))))); los disuelve las filas y crea una lista
 
 ;;elimina la profundidad
+
+;Descripcion: funcio que elimina la profuncidad de una imagen, eliminando el ultimo elemento de un pixel
+;Dominio: List
+;Recorrido: List
+;Tipo de recursion: Natural
+
 (define filtro-d
   (lambda (imagen-descontruida)
     (cond
       [(null? imagen-descontruida) '()]
       [else (cons (remove (last (car imagen-descontruida)) (car imagen-descontruida)) (filtro-d (cdr imagen-descontruida)))])))
 
+
+;Descripcion: funcio que llama a la funcion "filtro-d" y le entrega una imagen deconstruida
+;Dominio: Imagen
+;Recorrido: List
+
 (define usa-filtros-descontruidos
   (lambda (imagen)
-    (filtro-d(descontructor imagen))))
+    (filtro-d(descontructor imagen)))); le aplica la funcion deconstructor y se lo pasa como parametro a la funcion "filtro-d"
 
 ;;(constructor-imagen 2 2 (pixrgb-d  0 0 10 10 10 10) (pixrgb-d  0 1 20 20 20 20) (pixrgb-d 1 0 30 30 30 30) (pixrgb-d 1 1 40 40 40 40))
 
@@ -405,34 +513,35 @@
 
 (define depthLayers
   (lambda (imagen)
-    (depthLayers-seleciona imagen (descontructor imagen) '() '())))
+    (depthLayers-seleciona imagen (descontructor imagen) '() '() (que-pixel-es? (car (descontructor imagen))))))
 
 (define depthLayers-seleciona
-  (lambda (imagen image-decons lista-imagenes lista-revisados)
+  (lambda (imagen image-decons lista-imagenes lista-revisados tipo-pixel)
     (cond
       [(null? image-decons) lista-imagenes]
-      [(eq? #f (revisado (car image-decons) lista-revisados)) (depthLayers-seleciona imagen (cdr image-decons) (cons (crea-imagen-profu imagen '() (car image-decons)) lista-imagenes) (cons (car image-decons) lista-revisados))]
-      (else (depthLayers-seleciona imagen (cdr image-decons) lista-imagenes lista-revisados)))))
+      [(eq? #f (revisado-depth (car image-decons) lista-revisados)) (depthLayers-seleciona imagen (cdr image-decons)
+                                                                                           (cons (crea-imagen-depth imagen '() (car image-decons) tipo-pixel) lista-imagenes) (cons (car image-decons) lista-revisados) tipo-pixel)]
+      (else (depthLayers-seleciona imagen (cdr image-decons) lista-imagenes lista-revisados tipo-pixel)))))
 
-(define crea-imagen-profu
-  (lambda (imagen new-image profundidad)
+(define crea-imagen-depth
+  (lambda (imagen new-image profundidad tipo-pixel)
     (cond
       [(null? imagen) (reverse new-image)]
-      (else (crea-imagen-profu (cdr imagen) (cons (crea-filas-d (car imagen) '() profundidad) new-image) profundidad)))))
+      (else (crea-imagen-depth (cdr imagen) (cons (crea-filas-d (car imagen) '() profundidad tipo-pixel) new-image) profundidad tipo-pixel)))))
 
 (define crea-filas-d
-  (lambda (fila lista-aux profundidad)
+  (lambda (fila lista-aux profundidad tipo-pixel)
     (cond
       [(null? fila) (reverse lista-aux)]
-      [(equal? (last profundidad) (last (car fila))) (crea-filas-d (cdr fila) (cons (car fila) lista-aux) profundidad)]
-      (else (crea-filas-d (cdr fila) (cons (list 1) lista-aux) profundidad)))))
+      [(equal? (last profundidad) (last (car fila))) (crea-filas-d (cdr fila) (cons (car fila) lista-aux) profundidad tipo-pixel)]
+      (else (crea-filas-d (cdr fila) (cons tipo-pixel lista-aux) profundidad tipo-pixel)))))
 
-(define revisado
+(define revisado-depth
   (lambda (pixel lista-pixeles)
     (cond
       [(null? lista-pixeles) #f]
       [(eq? (last pixel) (last (car lista-pixeles))) #t]
-      (else (revisado pixel (cdr lista-pixeles))))))
+      (else (revisado-depth pixel (cdr lista-pixeles))))))
 
 ;(depthLayers img1)
 
@@ -443,6 +552,15 @@
 
 (define img50 '(((1 10) (0 10))
                ((0 20) (1 20))))
+
+(define que-pixel-es?
+  (lambda (pixel)
+    (cond
+      [(eq? (cant-elementos-pixhex pixel) 2)
+       (cond
+         [(number? (car pixel)) (list 1)]
+         (else (list "#FFFFFF")))]
+      (else (list 255 255 255)))))
 
 
 
