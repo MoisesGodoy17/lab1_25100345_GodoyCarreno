b61f5b95e959fe4901b95cb73ea254418b9ff7e8
MoisesGodoy17 <moises.godoy@usach.cl>
2022-09-26 23:53:52 -0300

    TDA - image Godoy 25100345

--- TDA-pixbit-d.rkt
+++ TDA-pixbit-d.rkt
@@ -3,6 +3,10 @@
 (provide pixbit-d)
 (provide es-pixbit?)
 
+;Descripcion: funcion que construye un pixbit-d.
+;Dominio: Num X Num X Num X Num.
+;Recorrido: Pibit-d.
+
 (define pixbit-d ;;contructor del bit de la imagen 
   (lambda (x y bit depth)
     (list x y bit depth)))
